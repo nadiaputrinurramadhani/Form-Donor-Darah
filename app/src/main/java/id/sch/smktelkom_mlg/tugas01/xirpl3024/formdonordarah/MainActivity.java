12e90082328cb4cb2a4bb75b388ccf8c1e575f06
nadia putri <nadiaputri395@gmail.com>
2016-09-11 16:22:25 +0700

    atur view

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3024/formdonordarah/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3024/formdonordarah/MainActivity.java
@@ -2,8 +2,18 @@ package id.sch.smktelkom_mlg.tugas01.xirpl3024.formdonordarah;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etnama;
+    EditText etTL;
+    EditText etAlamat;
+    EditText etNo;
+    Button buttonDaftar;
+    TextView textViewhasil;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
