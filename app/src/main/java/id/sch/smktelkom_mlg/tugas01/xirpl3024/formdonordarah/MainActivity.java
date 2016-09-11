5e9430853faaab973c77a48a84c95b0022ca0263
nadia putri <nadiaputri395@gmail.com>
2016-09-11 16:30:06 +0700

    deklarasi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3024/formdonordarah/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3024/formdonordarah/MainActivity.java
@@ -3,15 +3,26 @@ package id.sch.smktelkom_mlg.tugas01.xirpl3024.formdonordarah;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText etnama;
-    EditText etTL;
-    EditText etAlamat;
-    EditText etNo;
-    Button buttonDaftar;
+    EditText ettl;
+    EditText etalamat;
+    EditText ettel;
+    RadioGroup rg1;
+    RadioButton rblaki;
+    RadioButton rbpr;
+    Spinner spinnergoldar;
+    CheckBox cbpel;
+    CheckBox cbmaha;
+    CheckBox cbpeg;
+    Button bDaftar;
     TextView textViewhasil;
 
 
